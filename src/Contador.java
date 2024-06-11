82ba036f20e21d0c0fef4bdfc38206d6cea50fd9
misteregis <misteregis@gmail.com>
2024-06-11 20:59:50 -0300

    Projeto finalizado!
    O que foi feito:
     • Criado método 'contar' — usado para obter a quantidade de interações (for) e realizar a impressão no console;
     • Criado método 'solicitarInformacao' — usado para obter informações como o primeiro e segundo número;
     • Criado método 'limparConsole' — usado para limpar o console a cada solicitação;

--- src/Contador.java
+++ src/Contador.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class Contador {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            int primeiroNumero = Integer.parseInt(solicitarInformacao(scanner, "Digite o primeiro número:"));
+            int segundoNumero = Integer.parseInt(solicitarInformacao(scanner, "Digite o segundo número:"));
+
+            if (primeiroNumero >= segundoNumero) {
+                throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+            }
+
+            contar(primeiroNumero, segundoNumero);
+        } catch (ParametrosInvalidosException e) {
+            limparConsole();
+
+            System.out.println(e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+
+    private static void contar(int primeiroNumero, int segundoNumero) throws ParametrosInvalidosException {
+        limparConsole();
+
+        for (int i = 1; i <= (segundoNumero - primeiroNumero); i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+
+    private static String solicitarInformacao(Scanner scanner, String message) {
+        limparConsole();
+
+        System.out.println(message);
+
+        return scanner.nextLine();
+    }
+
+    private static void limparConsole() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
